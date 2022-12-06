8f938dbc3e2fd71510b69f87f0840ef10e4d3304
Greed19 <aldriep.antonio@gmail.com>
2022-12-06 23:51:07 +0800

    push for merge

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 // import TodoList from './ReactProjects/todolist/todolistv1/todolist';
 import Project01 from './reactPortfolio/projectEradicator/project01/project01.jsx'
 
-
+// for merge
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
